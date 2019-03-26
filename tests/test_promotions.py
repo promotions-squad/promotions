814c6c45d2c8523908d4ed34137a6a07f52296ae
Chaoyun Bao <cb4084@stern.nyu.edu>
2019-03-26 00:02:31 -0400

    added a few general test cases

--- tests/test_promotions.py
+++ tests/test_promotions.py
@@ -16,3 +16,32 @@ DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+class TestPromotions(unittest.TestCase):
+    """ Test Cases for Promotions """
+    
+    @classmethod
+    def setUpClass(cls):
+        """ These run once per Test suite """
+        app.debug = False
+        # Set up the test database
+        app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        Promotion.init_db(app)
+        db.drop_all()    # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == '__main__':
+    unittest.main()
