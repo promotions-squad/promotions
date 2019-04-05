c5fda7737670d5c2e8af3a132370d58df1fe93c2
scotthalperin <sphalperin@gmail.com>
2019-04-05 17:39:33 -0400

    Update promotion_factory.py

--- tests/promotion_factory.py
+++ tests/promotion_factory.py
@@ -15,7 +15,7 @@ class PromotionFactory(factory.Factory):
     productid = factory.Faker('product_id')
     category = FuzzyChoice(choices=['dollar', 'percentage', 'BOGO', 'BOHO'])
     available = FuzzyChoice(choices=[True, False])
-    discount = FuzzyInteger(50)
+    discount = factory.FuzzyInteger(50)
 #    startdate = ff.FuzzyDate(datetime.date.today()-datetime.timedelta(days=10),
 #                              datetime.date.today())
 #    enddate = ff.FuzzyDate(datetime.date.today(),
