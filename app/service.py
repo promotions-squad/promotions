32f8a5ff5d5e6c8cf1bb31b11b34aca88499c6be
scotthalperin <sphalperin@gmail.com>
2019-04-05 18:58:52 -0400

    Update service.py

--- app/service.py
+++ app/service.py
@@ -48,36 +48,53 @@ class DataValidationError(ValueError):
 ######################################################################
 @app.errorhandler(DataValidationError)
 def request_validation_error(error):
-    message = str(error)
-    app.logger.info(message)
-    return jsonify(status=400, error='Bad Request', message=message), \
-                   status.HTTP_400_BAD_REQUEST
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
 
-@app.errorhandler(404)
-def not_found(error):
-    message = str(error)
-    app.logger.info(message)
-    return jsonify(status=404, error='Not Found', message=message), \
-                   status.HTTP_404_NOT_FOUND
-
-@app.errorhandler(400)
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
-    message = str(error)
-    app.logger.info(message)
-    return jsonify(status=400, error='Bad Request', message=message), \
-                   status.HTTP_400_BAD_REQUEST
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = error.message or str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_400_BAD_REQUEST,
+                   error='Bad Request',
+                   message=message), status.HTTP_400_BAD_REQUEST
 
-@app.errorhandler(405)
-def method_not_allowed(error):
-    return jsonify(status=405, error='Method not Allowed',
-                   message='Your request method is not supported. Check your HTTP method and try again.'), \
-                   status.HTTP_405_METHOD_NOT_ALLOWED
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = error.message or str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_404_NOT_FOUND,
+                   error='Not Found',
+                   message=message), status.HTTP_404_NOT_FOUND
 
-@app.errorhandler(500)
-def internal_error(error):
-    return jsonify(status=500, error='Internal Server Error',
-                   message='Houston... we have a problem.'), \
-                   status.HTTP_500_INTERNAL_SERVER_ERROR
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = error.message or str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                   error='Method not Allowed',
+                   message=message), status.HTTP_405_METHOD_NOT_ALLOWED
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = error.message or str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                   error='Unsupported media type',
+                   message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = error.message or str(error)
+    app.logger.error(message)
+    return jsonify(status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                   error='Internal Server Error',
+                   message=message), status.HTTP_500_INTERNAL_SERVER_ERROR
 
 #Get Index
 @app.route('/')
