7546b7a20659adca8215f96ef34cdfda19714e60
elizaevesophia <eel262@stern.nyu.edu>
2019-04-25 16:20:12 -0400

    Update promotion_steps.py

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -70,14 +70,14 @@ def step_impl(context, button):
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
-    element = context.driver.find_element_by_id('search_results')
-    expect(element.text).to_contain(name)
-#     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-#         expected_conditions.text_to_be_present_in_element(
-#             (By.ID, 'search_results'),
-#            name
-#         )
-#     )
+#    element = context.driver.find_element_by_id('search_results')
+#    expect(element.text).to_contain(name)
+     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+         expected_conditions.text_to_be_present_in_element(
+             (By.ID, 'search_results'),
+            name
+         )
+     )
      expect(found).to_be(True)
 
 @then('I should not see "{name}" in the results')
@@ -88,14 +88,14 @@ def step_impl(context, name):
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-   element = context.driver.find_element_by_id('flash_message')
-   expect(element.text).to_contain(message)
-#    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-#         expected_conditions.text_to_be_present_in_element(
-#             (By.ID, 'flash_message'),
-#             message
-#       )
-#    )
+#   element = context.driver.find_element_by_id('flash_message')
+#   expect(element.text).to_contain(message)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+         expected_conditions.text_to_be_present_in_element(
+             (By.ID, 'flash_message'),
+             message
+       )
+    )
      expect(found).to_be(True)
 
 ##################################################################
