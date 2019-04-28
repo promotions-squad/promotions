0288668de8051f5dd6d61ad46be1217649f6b59c
elizaevesophia <eel262@stern.nyu.edu>
2019-04-28 16:53:50 -0400

    Update promotion_steps.py

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -118,15 +118,15 @@ def step_impl(context, text_string, element_name):
     )
     expect(found).to_be(True)
 
-#@when('I change "{element_name}" to "{text_string}"')
-#def step_impl(context, element_name, text_string):
-#    element_id = 'pet_' + element_name.lower()
+@when('I change "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = 'pet_' + element_name.lower()
 #    element = context.driver.find_element_by_id(element_id)
-#    # element = WebDriverWait(context.driver, WAIT_SECONDS).until(
-#    #     expected_conditions.presence_of_element_located((By.ID, element_id))
-#    # )
-#    element.clear()
-#    element.send_keys(text_string)
+    element = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+     )
+    element.clear()
+    element.send_keys(text_string)
 
 ## @when('I change "{key}" to "{value}"')
 ## def step_impl(context, key, value):
