8534a3bbcae889cabe416773c6e7ab7b3341dd0f
elizaevesophia <eel262@stern.nyu.edu>
2019-04-29 20:08:17 -0400

    Update promotion_steps.py

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -104,6 +104,18 @@ def step_impl(context, message):
 # prefixed by 'pet_' so the Name field has an id='pet_name'
 # We can then lowercase the name and prefix with pet_ to get the id
 ##################################################################
+@then('I should see "{text_string}" in the "{element_name}" field')
+def step_impl(context, text_string, element_name):
+     element_id = 'promotion_' + element_name.lower()
+#    element = context.driver.find_element_by_id(element_id)
+#    expect(element.get_attribute('value')).to_equal(text_string)
+     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+         expected_conditions.text_to_be_present_in_element_value(
+             (By.ID, element_id),
+             text_string
+         )
+     )
+     expect(found).to_be(True)
 
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
@@ -117,6 +129,7 @@ def step_impl(context, text_string, element_name):
          )
      )
      expect(found).to_be(True)
+
 
 @when('I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
