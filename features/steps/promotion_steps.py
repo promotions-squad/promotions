b886a1d1b9e0ea458d06edb6ff03609f95dd5405
elizaevesophia <eel262@stern.nyu.edu>
2019-04-29 20:38:49 -0400

    Revert "Update promotion_steps.py"
    This reverts commit 021ce59dd4697b51bc39cb19505ff9c1463c1361.

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -106,16 +106,30 @@ def step_impl(context, message):
 ##################################################################
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
-    element_id = 'promotion_' + element_name.lower()
+     element_id = 'promotion_' + element_name.lower()
 #    element = context.driver.find_element_by_id(element_id)
 #    expect(element.get_attribute('value')).to_equal(text_string)
-    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
          expected_conditions.text_to_be_present_in_element_value(
              (By.ID, element_id),
              text_string
-        )
-    )
-    expect(found).to_be(True)
+         )
+     )
+     expect(found).to_be(True)
+
+@then('I should see "{text_string}" in the "{element_name}" field')
+def step_impl(context, text_string, element_name):
+     element_id = 'promotion_' + element_name.lower()
+#    element = context.driver.find_element_by_id(element_id)
+#    expect(element.get_attribute('value')).to_equal(text_string)
+     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+         expected_conditions.text_to_be_present_in_element_value(
+             (By.ID, element_id),
+             text_string
+         )
+     )
+     expect(found).to_be(True)
+
 
 @when('I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
